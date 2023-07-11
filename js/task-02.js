6cc40a2bf764b46317f3bb3ef3404ff88a258968
PetrBrodeckiy <petr.brodeckiy@gmail.com>
2023-07-11 21:58:25 +0200

    task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,28 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const liElement = document.querySelector(`#ingredients`);
+const liArray = [];
+
+ingredients.forEach(ingredient => {
+  const item = document.createElement(`li`);
+  item.className = 'item';
+  item.textContent = ingredient;
+  liArray.push(item);
+})
+
+liElement.append(...liArray);
+// console.log(liElement);
+
+
+
+
+
+
+
+
+
+
+
+
