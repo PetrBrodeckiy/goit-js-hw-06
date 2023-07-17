0cef9702a09da9c23c0f31c510773b39e96c493c
PetrBrodeckiy <petr.brodeckiy@gmail.com>
2023-07-17 20:04:48 +0200

    task07InputEvt

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,6 @@
-const inputChange = document.querySelector('input#font-size-control')
-const abracadabra = document.querySelector('span#text')
+const inputChange = document.querySelector('input#font-size-control');
+const abracadabra = document.querySelector('span#text');
 
-inputChange.addEventListener('change', e => {
-    abracadabra.style.fontSize = `${e.target.value }px`
-})
+inputChange.addEventListener('input', (e) => {
+    abracadabra.style.fontSize = `${e.target.value}px`;
+});
